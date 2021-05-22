adca62b01ac9c68869293beac20de2708925b171
Ryan Mark <rgmark@gmail.com>
2021-05-21 17:08:47 -0700

    include

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var path = require('path');
 module.exports = {
   name: 'ember-collapsible-panel',
 
+  included: function() {
+    this._super.included.apply(this, arguments);
+  },
+
   config: function (env, baseConfig) {
     var configPath = path.join(this.root, 'config', 'environment.js');
 
